ea076181cb7da94af664c9e5108ae0cffa826332
Josh Weiss <joshuawe@gse7274657.attlocal.net>
2019-10-20 22:52:57 -0700

    routing array to view

--- public/js/index.js
+++ public/js/index.js
@@ -18,8 +18,33 @@
 //   });
 // });
 
+var curatedList = [];
+
+//grabs value from top dropdown
 $(function() {
-    $(".top-dd").change(function() {
+    $(".top-dd > select").change(function() {
         alert( $('option:selected', this).text() );
+        // if student state
+        //   then replace with new data
+        // else if sitution strate
+        //   then replace with new data
+        // set totaldata array
+        // else
+        //   "sorry, no matches found"
     });
+    // listen for side filter changes
+      // match with attributes, then replace curatedList
+    //for each item in array
+        //add tile
+
 });
+
+
+var data =    [
+                {name: "affirmation", description: "create sense of belonging", studentState: "timid", situationState: "independent work"},
+                {name: "chunking", description: "breaking up content", studentState: "anxious", situationState: "independent work"}
+              ]
+
+
+
+console.log(data);
--- routes/home.js
+++ routes/home.js
@@ -2,11 +2,21 @@
 
 var express = require('express');
 var router = express.Router();
+var data =    [
+                {name: "affirmation", description: "create sense of belonging", studentState: "timid", situationState: "independent work"},
+                {name: "chunking", description: "breaking up content", studentState: "anxious", situationState: "independent work"}
+
+var news = [];
+var this_news = {
+  'title': item.title,
+  'description': item.description
+}
+news.push(this_news);
 
 //render create page with create pug template
 router.get('/', (req, res, next)=>{
   res.render('home', {
-    //add most recent models (with Angular?)
+    newsi: JSON.stringify(news)
   });
 });
 
